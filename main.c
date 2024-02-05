0d5802e9b3735997513650c49d68250169ae6638
Rymdskora <105083997+Rymdskora@users.noreply.github.com>
2024-02-05 00:29:56 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+  char input[100];
+  char highestSale[10];
+  int saleAmount;
+  int grandTotal;
+  
+  
+  int salesPersonD = 0;
+  int salesPersonE = 0;
+  int salesPersonF = 0;
+  
+  
+  printf("Enter salesperson's initial or 'Z' to quit: ");
+  scanf("%s", input);
+
+
+  while (strcmp(input, "Z") != 0) {
+    
+    if (strcmp(input, "D") == 0 || strcmp(input, "d") == 0) {
+      printf("\nEnter sale amount for salesperson 'D': ");
+      scanf("%s", input);
+      salesPersonD += atoi(input);
+    }
+    else if (strcmp(input, "E") == 0|| strcmp(input, "e") == 0) {
+      printf("\nEnter sale amount for salesperson 'E': ");
+      scanf("%s", input);
+      salesPersonD += atoi(input);
+    }
+    else if (strcmp(input, "F") == 0 || strcmp(input, "f") == 0) {
+      printf("\nEnter sale amount for salesperson 'F': ");
+      scanf("%s", input);
+      salesPersonD += atoi(input);
+    }
+    else {
+      printf("\nError, invalid salesperson selected, please try again.");
+    }
+
+    printf("\nEnter salesperson initial or 'Z' to quit: ");
+    scanf("%s", input);
+  }
+
+  // We COULD bubblesort but I'm lazy!
+  if (salesPersonD > salesPersonE && salesPersonD > salesPersonE) {
+    strcpy(highestSale, "D");
+  }
+  else if (salesPersonE > salesPersonD && salesPersonE > salesPersonF) {
+    strcpy(highestSale, "D");
+  }
+  else if (salesPersonF > salesPersonD && salesPersonF > salesPersonE) {
+    strcpy(highestSale, "D");
+  }
+  
+  grandTotal = salesPersonD + salesPersonE + salesPersonF;
+  printf("\nGrand Total: $%d", grandTotal);
+  printf("\nHighest Sale: %s", highestSale);
+  
+  return 0;
+}
